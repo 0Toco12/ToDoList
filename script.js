4f3974778afe26d39ba9e8b1b5893c2c2eae3241
Gui Sanches <guilherme.sanches.mag@gmail.com>
2024-08-15 18:38:43 -0300

    adicionando responsividade

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const btnAdicionar = document.querySelector("#btnAdicionar");
 //seleciona o botão adicionar
 
-btnAdicionar.addEventListener("click",adicionar);
+btnAdicionar.addEventListener("click", adicionar);
 //fica prestando a atenção na tela, caso o botão seja clicado.
 //se botão for clicado, irá chamar a função adicionar...
 
-function adicionar(){
+function adicionar() {
     const item = document.createElement("li");
     //cria o elemento "li" e o coloca na variável item
     const caixaValor = document.querySelector("#minhaCaixa").value;
@@ -14,7 +14,7 @@ function adicionar(){
     //insire o texto capturado na variável e o insere nesta nova variavel
     item.appendChild(novoNome);
     //adiciona o elemento li no elemento ul
-    if (caixaValor === ""){ //verifica se a caixa está vázia
+    if (caixaValor === "") { //verifica se a caixa está vázia
         alert("É necessário digitar algum texto para adicionar")
         //dá aviso caso estej vazio
     } else { //senão
@@ -33,25 +33,23 @@ function adicionar(){
 
     const close = document.querySelectorAll(".fechar");
     //captura toas as spans que tem classe com nome "fechar"
-    for(let i=0; i < close.length; i++) {
+    for (let i = 0; i < close.length; i++) {
         //vare a lista da spans e verifica uma por uma, para verificar qual dela foi clicado
-        close[i].addEventListener("click",apagar)
+        close[i].addEventListener("click", apagar)
         //o item clicado, chama a função apagar
     }
 }
 
-function apagar(){
+function apagar() {
     const item = this.parentElement;
     //captura a lista inteira
     item.style.display = "none";
     //se o nome do item que eu cliquei na lista foi"li"
-function checar(ev){
-    if(ev.target.nodeName == "li"){
-    //verifica se o item clicado é uma li
-    ev.target.classList.toggle("checado");
-    //adiciona nesse elemento clicado a classe "chegado"
+    function checar(ev) {
+        if (ev.target.nodeName == "li") {
+            //verifica se o item clicado é uma li
+            ev.target.classList.toggle("checado");
+            //adiciona nesse elemento clicado a classe "chegado"
+        }
     }
-
-    }
-    
 }
